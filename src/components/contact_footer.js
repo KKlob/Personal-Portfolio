b1a47adc9e396f7cb42fb4494c9ffeff7ad4d933
KKlob <klobk23@gmail.com>
2023-05-02 17:59:10 -0400

    adjusted styling for contact_footer

--- src/components/contact_footer.js
+++ src/components/contact_footer.js
@@ -9,13 +9,19 @@ export default function ContactFooter () {
     return (
         <Flex 
             width={{ base: "100vw", md: "80vw", lg: "60vw" }}
+            marginTop="10px"
         >
             <Spacer />
             <Link
                 href="https://www.linkedin.com/in/kainklob/"
                 rel="noopener noreferrer"
                 target="_blank"
-                style={{ color: "white" }}
+                color="white"
+                bg="gray"
+                borderRadius="25px"
+                paddingLeft="15px"
+                paddingRight="15px"
+                border="1px solid white"
             >
                 <Text color="white">LinkedIn</Text>
             </Link>
@@ -24,7 +30,12 @@ export default function ContactFooter () {
                 href="https://www.github.com/KKlob"
                 rel="noopener noreferrer"
                 target="_blank"
-                style={{ color: "white" }}
+                color="white"
+                bg="gray"
+                borderRadius="25px"
+                paddingLeft="15px"
+                paddingRight="15px"
+                border="1px solid white"
             >
                 <Text color="white">GitHub</Text>
             </Link>
